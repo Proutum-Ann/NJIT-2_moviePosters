ec3513a55404a1bf32fb451bc9743cb58d8ac409
Proutum-Ann <154941331+Proutum-Ann@users.noreply.github.com>
2025-12-01 13:50:06 +0000

    added vue javascript

--- site.js
+++ site.js
@@ -34,12 +34,11 @@ const vue_app = Vue.createApp({
             // This holds your movies.json data.
             movies: [],
             /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
-         
       }
     },
-      methods: {
-            /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
-      }
+    methods: {
+      /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
+    }
 })
 
 vue_app.mount("#vue_app")
